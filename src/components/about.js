71585331078f414d11c629f70ddd13896c8fafb9
Aymen Derouiche <a.derouiche@workflow-dev.fr>
2023-12-05 12:12:38 +0000

    BIG commit

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,5 @@
 import React from "react";
-import data from "../data";
+import {data} from "../data";
 import { SlLocationPin } from "react-icons/sl";
 import { IoMdPhonePortrait } from "react-icons/io";
 import { MdDriveFileRenameOutline } from "react-icons/md";
